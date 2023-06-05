8e0d891a5627929ac055dff35e3218ad733e98d4
ChazPollino <132384915+ChazPollino@users.noreply.github.com>
2023-06-04 23:11:57 -0400

    Add files via upload

---
+++ Activity 11 practice 2/Activity 11 practice 2/Dice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Chaz Pollino
+//CST 150
+// 6/4/23
+
+//THIS IS MY OWN WORK
+
+namespace Activity_11_practice_2
+    
+{
+    public class Dice
+    {
+        private int NUMBER_OF_SIDES;
+        private static Random Dice_Face = new Random();
+
+        public Dice (int NUMBER_OF_SIDES)
+        {
+            if (NUMBER_OF_SIDES > 4 || NUMBER_OF_SIDES > 20)
+            {
+                Console.WriteLine("Number Should Be Inbetween 4 and 20");
+
+            }
+            this.NUMBER_OF_SIDES = NUMBER_OF_SIDES;
+        }
+
+        public int rollDice()
+        {
+
+            return Dice_Face.Next(1, NUMBER_OF_SIDES + 1);
+        }
+    }
+}
---
+++ Activity 11 practice 2/Activity 11 practice 2/Program.cs
@@ -0,0 +1,41 @@
+﻿using Activity_11_practice_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Chaz Pollino
+//CST 150
+// 6/4/23
+
+//THIS IS MY OWN WORK
+
+namespace Activtiy_11_practice_2
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Dice die1 = new Dice(6);
+            Dice die2 = new Dice(6);
+
+            int Dice1roll, Dice2roll;
+            int rolls = 0;
+
+
+
+            do
+            {
+                Dice1roll = die1.rollDice();
+                Dice2roll = die2.rollDice();
+                Console.WriteLine("Rolled [ " + "Die1: " + Dice1roll + " Die2: " + Dice2roll + " ]");
+                rolls++;
+
+            }
+            while (Dice1roll != 1 || Dice2roll != 1);
+            Console.WriteLine("\n It Took The User " + rolls + " Rolls To Get A Pair Of Snake Eyes.");
+            Console.ReadLine();
+        }
+    }
+}
